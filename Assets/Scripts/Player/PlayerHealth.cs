0cf74b2c86cea1a3841b92be3c995208d1351ddc
bruhenri12 <bhsm@cin.ufpe.br>
2022-10-26 22:18:44 -0300

    Menu, GameOver, and Hearts(unfinished)

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,24 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
     private float health = 0f;
     [SerializeField] private float maxHealth = 100f;
+    private int heart;
+
+    private GameObject healthObject;
 
     private void Start(){
         health = maxHealth;
+        heart = 9;
+        healthObject = GameObject.Find("Health");
     }
 
     public void UpdateHealth(float modifier){
         health += modifier;
+        if (modifier == -10f){
+            healthObject.GetComponent<Health>().RemoveHeart(heart);
+            heart -= 1;
+        }
         if (health > maxHealth){
             health = maxHealth;
         } else if (health <= 0f){
             health = 0f;
             
-            Debug.Log("Player is Dead"); //Fazer depois a morte do player propriamente
-        }
+            // Debug.Log("Player is Dead"); //Fazer depois a morte do player propriamente
+            SceneManager.LoadScene("GameOver");
+        } 
     }
 }
