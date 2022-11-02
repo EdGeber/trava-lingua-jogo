623a76db8e39b72872d8a613b8d05b4babbe220d
bruhenri12 <bhsm@cin.ufpe.br>
2022-11-02 15:46:32 -0300

    main down hud layout

--- Assets/Scripts/AudioInputManager.cs
+++ Assets/Scripts/AudioInputManager.cs
@@ -24,7 +24,6 @@ public class AudioInputManager : MonoBehaviour
     set { startRecognising = value; }
   }
 
-  private bool hasStarted = false;
   private string[] travaLinguaArray;
   private bool gameIsPaused = PauseBehaviour.GameIsPaused;
   // Start is called before the first frame update
