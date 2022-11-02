ca7e650582e8cdbb1dfd076e88aa5ec1e361ad07
Matheus Paixão <mpgs@cin.ufpe.br>
2022-11-02 20:58:00 -0300

    added audio files and sound fx manager script

---
+++ Assets/Scripts/SoundEffectsManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffectsManager : MonoBehaviour
+{
+  public static SoundEffectsManager instanceSEM { get; private set; }
+  [SerializeField] public AudioSource slowAudioSource;
+  [SerializeField] public AudioSource freezeAudioSource;
+  [SerializeField] public AudioSource bigDamageAudioSource;
+  [SerializeField] public AudioSource healAudioSource;
+  [SerializeField] public AudioSource bombAudioSource;
+
+  private void Awake()
+  {
+    instanceSEM = this;
+  }
+
+  public void playSlowAudio()
+  {
+    this.slowAudioSource.Play();
+  }
+
+  public void playFreezeAudio()
+  {
+    this.freezeAudioSource.Play();
+  }
+
+  public void playBigDamageAudio()
+  {
+    this.bigDamageAudioSource.Play();
+  }
+
+  public void playHealAudio()
+  {
+    this.healAudioSource.Play();
+  }
+
+  public void playBombAudio()
+  {
+    this.bombAudioSource.Play();
+  }
+}
